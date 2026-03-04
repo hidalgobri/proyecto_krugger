2a071ea22fe9b0c6deb5c7d3d7a517b21f2a350f
Valeria Hidalgo <valeria.hidalgo@humana.med.ec>
2026-03-04 07:02:00 -0500

    front added

---
+++ material-kit-react/src/layouts/nav-config-dashboard.tsx
@@ -0,0 +1,50 @@
+import { Label } from 'src/components/label';
+import { SvgColor } from 'src/components/svg-color';
+
+// ----------------------------------------------------------------------
+
+const icon = (name: string) => <SvgColor src={`/assets/icons/navbar/${name}.svg`} />;
+
+export type NavItem = {
+  title: string;
+  path: string;
+  icon: React.ReactNode;
+  info?: React.ReactNode;
+};
+
+export const navData = [
+  {
+    title: 'Home',
+    path: '/',
+    icon: icon('ic-home'),
+  },
+  {
+    title: 'User',
+    path: '/user',
+    icon: icon('ic-user'),
+  },
+  {
+    title: 'Product',
+    path: '/products',
+    icon: icon('ic-cart'),
+
+  },
+
+  {
+    title: 'Blog',
+    path: '/blog',
+    icon: icon('ic-glass-buy'),
+  },
+/*
+  {
+    title: 'Sign in',
+    path: '/sign-in',
+    icon: icon('ic-lock'),
+  },
+  {
+    title: 'Not found',
+    path: '/404',
+    icon: icon('ic-disabled'),
+  },
+*/
+];
---
+++ material-kit-react/src/sections/overview/view/overview-analytics-view.tsx
@@ -0,0 +1,18 @@
+import Grid from '@mui/material/Grid';
+import Typography from '@mui/material/Typography';
+
+import { DashboardContent } from 'src/layouts/dashboard';
+import { _posts, _tasks, _traffic, _timeline } from 'src/_mock';
+
+// ----------------------------------------------------------------------
+
+export function OverviewAnalyticsView() {
+  return (
+    <DashboardContent maxWidth="xl">
+      <Typography variant="h4" >
+        Hi, Welcome back 👋
+      </Typography>
+
+    </DashboardContent>
+  );
+}
